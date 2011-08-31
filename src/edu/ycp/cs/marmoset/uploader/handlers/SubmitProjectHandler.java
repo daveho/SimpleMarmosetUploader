589588f28cca3bb91d13a09a2c13b01204e11246
David Hovemeyer <david.hovemeyer@gmail.com>
2011-08-31 15:10:38 -0400

    ensure 1 project is selected, check for existence of .submit file

--- src/edu/ycp/cs/marmoset/uploader/handlers/SubmitProjectHandler.java
+++ src/edu/ycp/cs/marmoset/uploader/handlers/SubmitProjectHandler.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.ui.ISelectionService;
@@ -44,13 +45,31 @@ public class SubmitProjectHandler extends AbstractHandler {
 			selectedProjects = getSelectedProjects((IStructuredSelection) selection);
 		}
 
-		if (selectedProjects == null) {
-			MessageDialog.openError(window.getShell(), "No project selected", "Please select a project in the package explorer.");
-		} else {
-			MessageDialog.openInformation(
+		if (selectedProjects == null || selectedProjects.isEmpty()) {
+			MessageDialog.openError(
 					window.getShell(),
-					"SimpleMarmosetUploader",
-					"Found " + selectedProjects.size() + " selected projects");
+					"No project selected",
+					"Please select a project in the package explorer.");
+		} else if (selectedProjects.size() > 1) {
+			MessageDialog.openError(
+					window.getShell(),
+					"Multiple projects selected",
+					"Please select a single project in the package explorer.");
+		} else {
+			IProject project = selectedProjects.get(0);
+			
+			IFile dotSubmit = project.getFile(".submit");
+			if (!dotSubmit.exists()) {
+				MessageDialog.openError(
+						window.getShell(),
+						"No submit file",
+						"This project does not contain project submission information.  Sorry.");
+			} else {
+				MessageDialog.openInformation(
+						window.getShell(),
+						"SimpleMarmosetUploader",
+						"Found " + selectedProjects.size() + " selected projects");
+			}
 		}
 		
 		return null;
