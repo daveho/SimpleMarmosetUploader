8b2322aca3ada0340947171f397362eceec7104f
David Hovemeyer <david.hovemeyer@gmail.com>
2011-09-01 13:14:10 -0400

    ignore gen. files, scripts to publish update site

---
+++ installUpdateSite.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+if [ ! -r updateSite.zip ]; then
+	echo "No update site!"
+	exit 1
+fi
+
+scp updateSite.zip dhovemey@cs.ycp.edu: && ssh cs.ycp.edu 'cd public_html && mkdir -p simpleMarmosetUploader && cd simpleMarmosetUploader && unzip ~/updateSite.zip'
---
+++ zipUpdateSite.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+(cd SimpleMarmosetUploaderUpdateSite && zip -9r ../updateSite.zip *)
