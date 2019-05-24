668ad100ca6ef7ec55cec85f6571956ec2dcb14b
David Hovemeyer <david.hovemeyer@gmail.com>
2019-05-24 10:42:26 -0400

    check in update site to gh-pages

--- installUpdateSite.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if [ ! -r updateSite.zip ]; then
-	echo "No update site!"
-	exit 1
-fi
-
-scp updateSite.zip dhovemey@cs.ycp.edu: && ssh cs.ycp.edu 'cd public_html && mkdir -p simpleMarmosetUploader && cd simpleMarmosetUploader && unzip -o ~/updateSite.zip'
--- zipUpdateSite.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-(rm -rf updateSite.zip && \
-	cd SimpleMarmosetUploaderUpdateSite && \
-	zip -9r ../updateSite.zip features plugins site.xml)
